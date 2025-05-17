420f71d96216f2aeefe43ce6f8a7a115b829977d
dvdmarveira <ddmdoliveira@gmail.com>
2025-05-16 23:57:15 -0300

    add channel

--- main.go
+++ main.go
@@ -5,184 +5,197 @@ import (
 	"time"
 )
 
-type Cliente struct {
-	Nome     string
-	Idade    int
-	Endereco Endereco
-	Email    string
-}
-
-type Endereco struct {
-	Cidade string 
-	Estado string
-	Cep string
-}
-
-func (c Cliente) Apresentar() {
-	fmt.Printf("Olá, meu nome é %s e tenho %d anos.\n", c.Nome, c.Idade)
-}
-
 func main() {
-	var p1 Cliente = Cliente{Nome: "deyvid marques"}
-	var p2 Cliente = Cliente{Nome: "dvd marveira"}
-	fmt.Println(p1.Nome)
-	var p3 *Cliente = &p1 // definiu p3 como ponteiro de p1 passando como referência apontando o endereço.
+	ch := make(chan int, 3)
+	// ch <- 10 // escrevendo valor
+	// <-ch // lendo valor
 
-	fmt.Println(&p1.Nome)
-	p3.Nome = "David" // mudou o valor original de p1
-	fmt.Println(p1.Nome)
-	fmt.Println(p2.Idade)
-	fmt.Println(&p3.Nome)
+	go func() {
+		ch <- 3
+		ch <- 2
+		ch <- 2
+	}()
 
-	multiplicacao()
+	time.Sleep(time.Second * 1)
+	valor := <-ch
+	fmt.Println("valor do canal:", valor)
 }
 
-func multiplicacao() { // essa forma de declarar função permite que ela acesse variáveis globais
-	var fixo = 4
-	multiplica := func(x int) int {
-		return x * fixo
-	}
+// type Cliente struct {
+// 	Nome     string
+// 	Idade    int
+// 	Endereco Endereco
+// 	Email    string
+// }
 
-	resultado := multiplica(5)
-	fmt.Println(resultado)
+// type Endereco struct {
+// 	Cidade string
+// 	Estado string
+// 	Cep string
+// }
 
-	soma(2, 5)
-}
+// func (c Cliente) Apresentar() {
+// 	fmt.Printf("Olá, meu nome é %s e tenho %d anos.\n", c.Nome, c.Idade)
+// }
 
-func soma(a, b int) { // essa forma de declarar função não permite que ela acesse variáveis globais
-	fmt.Println(a + b)
+// func main() {
+// 	var p1 Cliente = Cliente{Nome: "deyvid marques"}
+// 	var p2 Cliente = Cliente{Nome: "dvd marveira"}
+// 	fmt.Println(p1.Nome)
+// 	var p3 *Cliente = &p1 // definiu p3 como ponteiro de p1 passando como referência apontando o endereço.
 
-	clientes()
-}
+// 	fmt.Println(&p1.Nome)
+// 	p3.Nome = "David" // mudou o valor original de p1
+// 	fmt.Println(p1.Nome)
+// 	fmt.Println(p2.Idade)
+// 	fmt.Println(&p3.Nome)
 
-func clientes() {
-	cliente1 := Cliente{
-		Nome: "Siri",
-		Idade: 19,
-		Endereco: Endereco {
-			Cidade: "Recife",
-			Estado: "Pernambuco",
-			Cep: "01010100",
-		},
-	}
+// 	multiplicacao()
+// }
 
-	cliente2 := Cliente{
-		Nome: "Iris",
-		Idade: 19,
-	}
+// func multiplicacao() { // essa forma de declarar função permite que ela acesse variáveis globais
+// 	var fixo = 4
+// 	multiplica := func(x int) int {
+// 		return x * fixo
+// 	}
 
-	cliente2.Email = "iris@email.com"
+// 	resultado := multiplica(5)
+// 	fmt.Println(resultado)
 
-	fmt.Println("Cliente 1:", cliente1)
-	fmt.Println("Cliente 2:", cliente2.Nome)
+// 	soma(2, 5)
+// }
 
-	people()
-}
+// func soma(a, b int) { // essa forma de declarar função não permite que ela acesse variáveis globais
+// 	fmt.Println(a + b)
 
-func people() {
-	var pessoas = map[string]int{}
-	pessoas["dvd"] = 23
-	pessoas["isis"] = 19
+// 	clientes()
+// }
 
-	if idade, ok := pessoas["dvd"]; ok {
-			fmt.Println("Pessoa existe no map", idade, ok)
-	} else {
-		fmt.Println("Pessoa não existe no map")
-	}
+// func clientes() {
+// 	cliente1 := Cliente{
+// 		Nome: "Siri",
+// 		Idade: 19,
+// 		Endereco: Endereco {
+// 			Cidade: "Recife",
+// 			Estado: "Pernambuco",
+// 			Cep: "01010100",
+// 		},
+// 	}
 
-	nota()
-}
+// 	cliente2 := Cliente{
+// 		Nome: "Iris",
+// 		Idade: 19,
+// 	}
 
-func nota() { 
-	nota := 50
-	if nota >= 90 {
-		fmt.Println("Aprovado com distinção")
-	} else if nota >= 70 {
-			fmt.Println("Aprovado")
-	} else {
-		fmt.Println("Reprovado")
-	}
+// 	cliente2.Email = "iris@email.com"
 
-	players()
-}
+// 	fmt.Println("Cliente 1:", cliente1)
+// 	fmt.Println("Cliente 2:", cliente2.Nome)
 
-func players() {
-	players := map[string]int{
-		"david": 15,
-	}
+// 	people()
+// }
 
-	if value, ok := players["david"]; ok {
-		fmt.Println(value, "pontos")
-	} else {
-		fmt.Println("Jogador não encontrado")
-	}
+// func people() {
+// 	var pessoas = map[string]int{}
+// 	pessoas["dvd"] = 23
+// 	pessoas["isis"] = 19
 
-	switchCase()
-}
+// 	if idade, ok := pessoas["dvd"]; ok {
+// 			fmt.Println("Pessoa existe no map", idade, ok)
+// 	} else {
+// 		fmt.Println("Pessoa não existe no map")
+// 	}
 
-func switchCase() {
-	fmt.Println("Quando é sexta-feira?")
-	today := time.Now().Weekday()
+// 	nota()
+// }
 
-	switch time.Friday {
-	case today + 0:
-		fmt.Println("é hoje")
-	case today + 1:
-		fmt.Println("é amanhã")
-	case today + 2:
-		fmt.Println("é em dois dias")
-	default:
-		fmt.Println("Ainda está longe...")
-	}
+// func nota() {
+// 	nota := 50
+// 	if nota >= 90 {
+// 		fmt.Println("Aprovado com distinção")
+// 	} else if nota >= 70 {
+// 			fmt.Println("Aprovado")
+// 	} else {
+// 		fmt.Println("Reprovado")
+// 	}
 
-	loopFor()
-}
+// 	players()
+// }
+
+// func players() {
+// 	players := map[string]int{
+// 		"david": 15,
+// 	}
+
+// 	if value, ok := players["david"]; ok {
+// 		fmt.Println(value, "pontos")
+// 	} else {
+// 		fmt.Println("Jogador não encontrado")
+// 	}
+
+// 	switchCase()
+// }
+
+// func switchCase() {
+// 	fmt.Println("Quando é sexta-feira?")
+// 	today := time.Now().Weekday()
+
+// 	switch time.Friday {
+// 	case today + 0:
+// 		fmt.Println("é hoje")
+// 	case today + 1:
+// 		fmt.Println("é amanhã")
+// 	case today + 2:
+// 		fmt.Println("é em dois dias")
+// 	default:
+// 		fmt.Println("Ainda está longe...")
+// 	}
+
+// 	loopFor()
+// }
+
+// // func loopFor() {
+// // 	sum := 0
+// // 	for i := 0; i < 10; i++{
+// // 		fmt.Println(i)
+// // 		sum += i
+// // 	}
+
+// // 	fmt.Println(sum)
+// // }
 
 // func loopFor() {
 // 	sum := 0
-// 	for i := 0; i < 10; i++{
-// 		fmt.Println(i)
-// 		sum += i
+// 	for sum < 20 {
+// 		fmt.Println("loop")
+// 		sum += 10
 // 	}
 
 // 	fmt.Println(sum)
+
+// 	sliceNums()
 // }
 
-func loopFor() {
-	sum := 0
-	for sum < 20 {
-		fmt.Println("loop")
-		sum += 10
-	}
+// func sliceNums() {
+// 	nums := []int{1, 2, 3, 4, 5}
+// 	for i := 0; i < len(nums); i++ {
+// 			fmt.Println(nums[i])
+// 	}
 
-	fmt.Println(sum)
+// 	ranges()
+// }
 
-	sliceNums()
-}
+// func ranges() {
+// 	nums := []string{"marveira", "marques", "oliveira"}
+// 	for key, value := range nums{
+// 		fmt.Println(key, value)
+// 	}
 
-func sliceNums() {
-	nums := []int{1, 2, 3, 4, 5}
-	for i := 0; i < len(nums); i++ {
-			fmt.Println(nums[i])
-	}
-
-	ranges()
-}
-
-func ranges() {
-	nums := []string{"marveira", "marques", "oliveira"}
-	for key, value := range nums{
-		fmt.Println(key, value)
-	}
-
-	users := map[string]string{
-		"nome": "Iris",
-		"sexo": "feminino",
-	}
-	for key, value := range users {
-		fmt.Println(key, value)
-	}
-}
-
-
-
+// 	users := map[string]string{
+// 		"nome": "Iris",
+// 		"sexo": "feminino",
+// 	}
+// 	for key, value := range users {
+// 		fmt.Println(key, value)
+// 	}
+// }
